2d51320a69855c9fb0c2579802a4566244e80357
Mark <mgalex12@icloud.com>
2022-05-20 09:12:48 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
+
+
+
+
 // Code your solution here
+let initialValue = 0;
+const totalBatteries = batteryBatches.reduce(
+        (previousValue, currentValue) => previousValue + currentValue,
+        initialValue
+    );
+
+
+
+
+
+
+
+// const array1 = [1, 2, 3, 4];
+
+// // 0 + 1 + 2 + 3 + 4
+// const initialValue = 0;
+// const sumWithInitial = array1.reduce(
+//   (previousValue, currentValue) => previousValue + currentValue,
+//   initialValue
+// );
+
+// console.log(sumWithInitial);
